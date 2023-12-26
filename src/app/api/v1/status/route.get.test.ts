ee58555fecbacd54d1e9a87c99385bad3ca78f86
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2023-12-26 18:55:21 +0100

    implement database status and test

--- src/app/api/v1/status/route.get.test.ts
+++ src/app/api/v1/status/route.get.test.ts
@@ -1,6 +1,60 @@
+import { ResponseBody } from "./route";
+
+const ENDPOINT = "http://localhost:3000/api/v1/status";
+
 describe("GET /api/v1/status", () => {
-  it("should return 200 OK", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+  let body: ResponseBody;
+  let response: Response;
+
+  beforeEach(async () => {
+    response = await fetch(ENDPOINT);
+    body = await response.json();
+  });
+
+  it("should return status 200", async () => {
     expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe("application/json");
+  });
+
+  it("should return updated_at with valid ISO Date", async () => {
+    expect(body).toHaveProperty("updated_at");
+    expect(body.updated_at).not.toBeNull();
+
+    const parsedUpdatedAt = new Date(body.updated_at).toISOString();
+
+    expect(body.updated_at).toEqual(parsedUpdatedAt);
+  });
+
+  describe("dependencies", () => {
+    it("it should return dependencies", async () => {
+      expect(body).toHaveProperty("dependencies");
+      expect(body.dependencies).not.toBeNull();
+    });
+
+    describe("database", () => {
+      let database: ResponseBody["dependencies"]["database"];
+
+      beforeEach(async () => {
+        database = body.dependencies.database;
+      });
+
+      it("should return pg.max_connections with valid number", async () => {
+        expect(database).toHaveProperty("max_connections");
+        expect(database.max_connections).not.toBeNull();
+        expect(database.max_connections).toBe(100);
+      });
+
+      it("should return pg.version with valid string", async () => {
+        expect(database).toHaveProperty("version");
+        expect(database.version).not.toBeNull();
+        expect(database.version).toBe("16.0");
+      });
+
+      it("should return pg.open_connections with valid number", async () => {
+        expect(database).toHaveProperty("open_connections");
+        expect(database.open_connections).not.toBeNull();
+        expect(database.open_connections).toBe(1);
+      });
+    });
   });
 });
--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -3,11 +3,41 @@ import { NextRequest, NextResponse } from "next/server";
 import * as database from "infra/database";
 
 export async function GET(request: NextRequest) {
-  const result = await database.query("SELECT NOW();");
+  const updated_at = new Date().toISOString();
 
-  console.log(result.rows);
+  const {
+    rows: [{ max_connections }],
+  } = await database.query("show max_connections;");
+
+  const {
+    rows: [{ server_version: version }],
+  } = await database.query("show server_version;");
+
+  const {
+    rows: [{ open_connections }],
+  } = await database.query(
+    `
+    select count(1)::integer as open_connections
+    from pg_stat_activity
+    where "datname" = $1;
+    `,
+    [process.env.POSTGRES_DB],
+  );
 
   return NextResponse.json({
-    status: "OK",
+    updated_at,
+    dependencies: {
+      database: {
+        max_connections: parseInt(max_connections),
+        open_connections,
+        version,
+      },
+    },
   });
 }
+
+export type ResponseBody = ReturnType<typeof GET> extends Promise<
+  NextResponse<infer T>
+>
+  ? T
+  : never;
